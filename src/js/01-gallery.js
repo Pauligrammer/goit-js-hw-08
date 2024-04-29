de26ee59b743ee360863c32ce8ddb6a121239c06
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-04-30 00:56:29 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+const arItems = galleryItems
+  .map(
+    (galleryItem) =>
+      `<li><a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}" 
+    alt="${galleryItem.description}"/></a></li>`
+  )
+  .join("");
+gallery.innerHTML = arItems;
+
+gallery.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  const lightbox = new SimpleLightbox(`.gallery a`, {
+    captions: true,
+    captionSelector: "img",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+  lightbox.open();
+});
 
 console.log(galleryItems);
