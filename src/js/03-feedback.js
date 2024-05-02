ecbfdc187f89b708d961ef99b6e1dde3ee5cc61c
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-05-02 09:53:33 +0200

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const emailInput = from.elements.email;
+const messageInput = form.elements.message;
+
+const formDataStr = localStorage.getItem("feedback-form-state");
+if (formDataStr) {
+    const formData = JSON.parse(formDataStr);
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+}
+
+form.addEventListener("input", throttle((ev) => {
+    const data = {
+        email: ev.target.elements.email.value,
+        message: ev.target.elements.message.value,
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}), 500);
+
+form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    console.log({
+    email: ev.target.elements.email.value,
+    message: ev.target.elements.message.value
+});
+localStorage.clear();
+form.reset(0);
+});
