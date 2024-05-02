4332e5722e09fb1de2da37a80cfe3516fd5ca61f
Pauligrammer <150374386+Pauligrammer@users.noreply.github.com>
2024-05-02 08:48:54 +0200

    video "

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
-import player from @vimeo/player;
-import throttle from lodash.throttle;
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
 
 const iframe = document.querySelector('iframe#vimeo-player');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const currentTime = localStorage.getItem("videoplayer-current-time");
 if (currentTime) {
